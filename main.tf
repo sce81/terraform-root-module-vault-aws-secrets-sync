a30014786342ff6ea1fcda024947ccd0bfd0f659
Simon Elliott <simon.elliott@hashicorp.com>
2025-12-04 14:16:39 +0000

    develop - bumped module version

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "mount-kvv2" {
 
 module "sync_destination" {
   source   = "app.terraform.io/HashiCorp_AWS_Org/secret-sync-destination/vault"
-  version  = "1.0.1"
+  version  = "1.0.2"
   name     = var.name
   env      = var.env
   role_arn = data.aws_iam_role.aws-vault-role.arn
